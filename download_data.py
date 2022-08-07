943455e0e5862a10d58ef56e4af9d61d6e769396
kevinjojo1024 <kevinjojo1024@gmail.com>
2022-08-08 00:41:31 +0530

    made a separate download_data.py

--- download_data.py
+++ download_data.py
@@ -1,9 +1,9 @@
 import pandas_datareader as pdr
 from datetime import datetime
 import pickle
-
-start=datetime(2000, 1, 1)
-end=datetime(2022, 8, 1)
+# it may be better to give start and end date as filename
+start=datetime(2017, 8, 7)
+end=datetime(2022, 8, 7)
 data = pdr.get_data_yahoo(symbols='INFY.NS', start=start, end=end, pause=0.1, chunksize=1, adjust_price=True)
 
 now = datetime.now()
@@ -12,6 +12,7 @@ foldername = "yahoo_data\\"
 filename = foldername + "infy_" + dt_str + ".pickle"
 with open(filename, 'wb') as f:
     pickle.dump(data, f, protocol=pickle.HIGHEST_PROTOCOL)
+    f.close()
 
 print("Stock data downloaded. Filename is :")
 print(filename)
